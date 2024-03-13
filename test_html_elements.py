a3035dc1c0f30fad3ae3d5306b91ce1873db73c0
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2024-03-13 13:07:44 -0400

    Update test_html_elements.py

--- test_html_elements.py
+++ test_html_elements.py
@@ -8,8 +8,8 @@ class TestH5Tag(unittest.TestCase):
     def setUp(self):
         # Setup Chrome options
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")  # Ensures the browser window does not open
-        #chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")  # Ensures the browser window does not open
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(options=chrome_options)
 
